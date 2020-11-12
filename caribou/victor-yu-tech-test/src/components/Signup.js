4ffd29eb827446ba865fd26d75958ac23e4511e4
Victor <victorcwyu@gmail.com>
2020-11-12 18:24:59 -0500

    Installed axios for handling requests, and completed Signup component.

--- caribou/victor-yu-tech-test/src/components/Signup.js
+++ caribou/victor-yu-tech-test/src/components/Signup.js
@@ -1,5 +1,7 @@
 import React, { useState, useContext } from "react";
 import UserContext from "../context/UserContext";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 export default function Signup() {
   const [userInfo, setUserInfo] = useState({
@@ -9,10 +11,41 @@ export default function Signup() {
     confirmPassword: "",
   });
   const { userData, setUserData } = useContext(UserContext);
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const signUp = await axios.post(
+        "http://localhost:5000/authentication/signup",
+        userInfo
+      );
+      // if sign up is a success, log in the user and navigate to home page
+      if (signUp.status === 201) {
+        const login = await axios.post(
+          "http://localhost:5000/authentication/login",
+          {
+            username: userInfo.username,
+            password: userInfo.password,
+          }
+        );
+        localStorage.setItem(
+          "authentication-token",
+          login.data.authenticationToken
+        );
+        const token = localStorage.getItem("authentication-token");
+        setUserData({ ...userData, token: token });
+        history.push("/");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
-    <div>
-      <form autoComplete="off">
+    <>
+      <h1>Sign Up</h1>
+      <form autoComplete="off" onSubmit={handleSubmit}>
         <input
           id="username"
           placeholder="username"
@@ -47,7 +80,8 @@ export default function Signup() {
             setUserInfo({ ...userInfo, confirmPassword: e.target.value })
           }
         />
+        <button type="submit">Sign up</button>
       </form>
-    </div>
+    </>
   );
 }
