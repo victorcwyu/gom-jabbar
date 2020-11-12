bec45375b29bd2911f0411bca0519baeeb576a1e
Victor <victorcwyu@gmail.com>
2020-11-12 01:31:55 -0500

    Created and implemented UserContext.

--- caribou/victor-yu-tech-test/src/App.js
+++ caribou/victor-yu-tech-test/src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import UserContext from "./context/UserContext";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={Home} />
-      <Route path="/signup" exact component={Signup} />
+      <UserContext.Provider>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={Signup} />
+      </UserContext.Provider>
     </Router>
   );
 }
