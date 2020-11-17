d2593ab64e36ab95da8eaae005509a8c231d12bf
Victor <victorcwyu@gmail.com>
2020-11-17 18:54:03 -0500

    Refactored NewReportMap.

--- caribou/victor-yu-tech-test/src/components/NewReportMap.js
+++ caribou/victor-yu-tech-test/src/components/NewReportMap.js
@@ -12,67 +12,66 @@ export default function NewReportMap() {
   const { userData, setUserData } = useContext(UserContext);
   const history = useHistory();
   const googleMapRef = useRef(null);
+  const token = localStorage.getItem("authentication-token");
 
   useEffect(() => {
-    if (!userData.user) {
+    if (!token) {
       history.push("/");
+    } else {
+      const createNewReportMap = () => {
+        // Initialize Google Maps
+        const map = initializeGoogleMap(googleMapRef.current);
+        // Initialize infoWindow
+        const pinInfoWindow = new window.google.maps.InfoWindow({
+          content: document.getElementById("info-content"),
+        });
+        // drops pin on map
+        const dropPin = (event) => {
+          const pin = new window.google.maps.Marker({
+            draggable: true,
+            position: event.latLng,
+            map: map,
+          });
+          getLatLong(pin);
+          // adds listener to open InfoWindow when pin is clicked
+          pin.addListener("click", function () {
+            getLatLong(pin);
+          });
+          // close InfoWindow until dragged to new position
+          pin.addListener("drag", function () {
+            pinInfoWindow.close();
+          });
+          // add an end of drag listener
+          pin.addListener("dragend", function () {
+            getLatLong(pin);
+          });
+          // remove listener so that the user can only drop one pin
+          window.google.maps.event.removeListener(pinListener);
+        };
+        // adds listener to drop pin when map is clicked
+        const pinListener = window.google.maps.event.addListener(
+          map,
+          "click",
+          dropPin
+        );
+        // retrieves latitude and longitude
+        function getLatLong(pin) {
+          let lat = pin.getPosition().lat();
+          let lng = pin.getPosition().lng();
+          // let latLong = pin.getPosition().lat() + "," + pin.getPosition().lng();
+          pinInfoWindow.open(map, pin);
+          buildIWContent(lat, lng);
+        }
+        // Load the pin information into the HTML elements used by the InfoWindow.
+        function buildIWContent(lat, lng) {
+          document.getElementById("iw-lat").textContent = lat;
+          document.getElementById("iw-lng").textContent = lng;
+        }
+      };
+      createNewReportMap();
     }
-  }, [userData.user, history]);
+  }, [token, history]);
 
-  useEffect(() => {
-    createNewReportMap();
-  }, []);
-
-  const createNewReportMap = () => {
-    // Initialize Google Maps
-    const map = initializeGoogleMap(googleMapRef.current);
-    // Initialize infoWindow
-    const pinInfoWindow = new window.google.maps.InfoWindow({
-      content: document.getElementById("info-content"),
-    });
-    // drops pin on map
-    const dropPin = (event) => {
-      const pin = new window.google.maps.Marker({
-        draggable: true,
-        position: event.latLng,
-        map: map,
-      });
-      getLatLong(pin);
-      // adds listener to open InfoWindow when pin is clicked
-      pin.addListener("click", function () {
-        getLatLong(pin);
-      });
-      // close InfoWindow until dragged to new position
-      pin.addListener("drag", function () {
-        pinInfoWindow.close();
-      });
-      // add an end of drag listener
-      pin.addListener("dragend", function () {
-        getLatLong(pin);
-      });
-      // remove listener so that the user can only drop one pin
-      window.google.maps.event.removeListener(pinListener);
-    };
-    // adds listener to drop pin when map is clicked
-    const pinListener = window.google.maps.event.addListener(
-      map,
-      "click",
-      dropPin
-    );
-    // retrieves latitude and longitude
-    function getLatLong(pin) {
-      let lat = pin.getPosition().lat();
-      let lng = pin.getPosition().lng();
-      // let latLong = pin.getPosition().lat() + "," + pin.getPosition().lng();
-      pinInfoWindow.open(map, pin);
-      buildIWContent(lat, lng);
-    }
-    // Load the pin information into the HTML elements used by the InfoWindow.
-    function buildIWContent(lat, lng) {
-      document.getElementById("iw-lat").textContent = lat;
-      document.getElementById("iw-lng").textContent = lng;
-    }
-  };
   // redirect to reports
   const submitReport = (e) => {
     e.preventDefault();
