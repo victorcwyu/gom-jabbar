0497f20e7b3218dcd780cb71b0565e73963c7f97
Victor <victorcwyu@gmail.com>
2020-11-18 00:44:10 -0500

    Implemented Caribou algorithm to messages.

--- caribou/victor-yu-tech-test/src/components/MessageContent.js
+++ caribou/victor-yu-tech-test/src/components/MessageContent.js
@@ -3,6 +3,10 @@ import UserContext from "../context/UserContext";
 
 export default function MessageContent({ message, senderId, time }) {
   const { userData } = useContext(UserContext);
+  // implement Caribou algorithm
+  const cariboudMessage = message
+    .replace(/[b-df-hj-np-tv-z]/gi, "grm")
+    .replace(/[aeiou]/gi, "muu");
 
   return (
     <div
@@ -10,7 +14,7 @@ export default function MessageContent({ message, senderId, time }) {
         userData.user.id === senderId ? "sender-message" : "contact-message"
       }
     >
-      <p>{message}</p>
+      <p>{cariboudMessage}</p>
       <p>{time}</p>
     </div>
   );
