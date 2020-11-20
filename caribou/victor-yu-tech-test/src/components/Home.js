3bd4e847194dad5ad3fc1bdd18ec7e7a16326bb0
Victor <victorcwyu@gmail.com>
2020-11-20 01:32:14 -0500

    Removed Header from Home.

--- caribou/victor-yu-tech-test/src/components/Home.js
+++ caribou/victor-yu-tech-test/src/components/Home.js
@@ -1,16 +1,24 @@
 import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import UserContext from "../context/UserContext";
 import Contacts from "./Contacts";
 import ReportsMap from "./ReportsMap";
-import Header from "./Header";
 
 export default function Home() {
   const { userData, setUserData } = useContext(UserContext);
+  const history = useHistory();
+
+  const handleLogOut = (e) => {
+    e.preventDefault();
+    localStorage.clear();
+    setUserData({ ...userData, token: null });
+    history.push("/");
+  };
 
   return (
     <>
-      <Header />
       <h1>Welcome back {userData.user.username}!</h1>
+      <button onClick={handleLogOut}>Log Out</button>
       <Contacts />
       <ReportsMap />
     </>
