b5f3ebc1f66ad860ca0b24987378ad08b9774535
Victor <victorcwyu@gmail.com>
2020-11-22 22:53:09 -0500

    Updated initializeGoogleMap function so that the maps begin centred in Montreal rather than Toronto.

--- caribou/victor-yu-tech-test/src/helpers/helpers.js
+++ caribou/victor-yu-tech-test/src/helpers/helpers.js
@@ -1,6 +1,6 @@
 const initializeGoogleMap = (ref) => {
   return new window.google.maps.Map(ref, {
-    center: { lat: 43.6560811, lng: -79.3823601 },
+    center: { lat: 45.508888, lng: -73.561668 },
     zoom: 14,
     disableDefaultUI: true,
   });
